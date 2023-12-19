755c62f67406ec596f882ff290d075349d25900d
santanna106 <gabriel.santana@gmail.com>
2023-12-19 06:02:08 -0300

    feat: desenvolvimento da página Sobre

---
+++ Sobre.py
@@ -0,0 +1,39 @@
+import streamlit as st 
+
+
+st.title('🏢 App ')
+
+st.header('Descrição do Aplicativo')
+st.write(
+    """
+    A análise automática de crédito é uma prática fundamental no setor financeiro e tem uma grande importância devido a diversos motivos, que vão desde a eficiência operacional até a tomada de decisões mais precisas e justas. Aqui estão algumas das razões pelas quais a análise automática de crédito é tão relevante:
+
+Eficiência e Velocidade: Uma das maiores vantagens da análise automática de crédito é a sua capacidade de processar grandes volumes de solicitações de crédito em tempo real. Isso acelera consideravelmente o processo de aprovação de empréstimos ou financiamentos, permitindo que os clientes obtenham respostas rápidas sobre suas solicitações.
+
+Redução de Custo: A automação da análise de crédito reduz significativamente os custos operacionais associados à contratação de pessoal para avaliar manualmente cada solicitação. Isso é especialmente relevante para instituições financeiras que precisam otimizar seus recursos.
+
+Precisão e Consistência: Os algoritmos de análise de crédito são projetados para tomar decisões com base em critérios objetivos e consistentes. Isso minimiza o risco de viés humano ou erros de julgamento, tornando as decisões mais confiáveis.
+
+Ampla Análise de Dados: Os modelos de análise de crédito automáticos podem processar uma ampla variedade de informações, incluindo histórico de pagamento, dados financeiros, histórico de emprego e muito mais. Isso permite uma avaliação mais abrangente do risco de crédito do solicitante.
+
+Personalização de Ofertas: Com a análise automática de crédito, as instituições financeiras podem personalizar ofertas de crédito com base no perfil do cliente. Isso ajuda a atender às necessidades individuais dos clientes e a criar relacionamentos mais sólidos.
+
+Redução de Fraudes: A automação da análise de crédito também ajuda na detecção precoce de fraudes, uma vez que pode identificar anomalias nos dados do solicitante.
+
+Inclusão Financeira: A análise automática de crédito pode ser mais inclusiva, permitindo que pessoas com históricos de crédito limitados ou inexistentes tenham acesso a serviços financeiros, o que é especialmente importante em economias em desenvolvimento.
+
+Compliance Regulatório: A automação ajuda as instituições financeiras a cumprir regulamentos rigorosos relacionados à concessão de crédito, garantindo que todas as regras e regulamentos sejam seguidos consistentemente.
+
+Tomada de Decisões Baseada em Dados: A análise automática de crédito permite que as decisões de crédito sejam baseadas em dados objetivos, o que é crucial para a sustentabilidade financeira das instituições.
+
+Melhoria da Experiência do Cliente: A rapidez e a conveniência associadas à análise automática de crédito contribuem para uma experiência mais positiva do cliente, o que é fundamental para a fidelização.
+
+Em resumo, a análise automática de crédito desempenha um papel fundamental na eficiência, precisão e acessibilidade dos serviços financeiros. Ela não apenas beneficia as instituições financeiras, tornando-as mais eficazes e eficientes, mas também pode beneficiar os consumidores, tornando o acesso ao crédito mais acessível e conveniente.
+    """) 
+
+st.subheader('Assistente')
+
+st.subheader('Modelo de Análise de Crédito')
+
+st.subheader('Equipe')
+
--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st 
-
-
-st.header('💜 App do Pessoal do MPBA')
